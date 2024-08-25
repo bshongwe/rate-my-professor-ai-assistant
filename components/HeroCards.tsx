9eabd70490c6cf09e9f0db058323d906ac3b5e11
bshongwe <dru.da.beat.hustle@gmail.com>
2024-08-25 04:38:33 +0200

    Logo

--- components/HeroCards.tsx
+++ components/HeroCards.tsx
@@ -43,7 +43,7 @@ export const HeroCards = () => {
             <Card className="absolute right-[20px] top-4 w-80 flex flex-col justify-center items-center drop-shadow-xl shadow-black/10 dark:shadow-white/10">
                 <CardHeader className="mt-8 flex justify-center items-center pb-2">
                     <Image
-                        src="/myavatar.webp"
+                        src="/saasfly-logo.svg"
                         alt="user avatar"
                         width={100}
                         height={100}
