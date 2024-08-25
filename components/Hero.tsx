53dabd3566281b24e0843acddb5e029742133263
bshongwe <dru.da.beat.hustle@gmail.com>
2024-08-25 02:57:59 +0200

    Update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
                         </span>{" "}
                         Professor
                     </h1>{" "}
-                    for{" "}
+                    v1{" "}
                     <h2 className="inline">
                         <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
                             AI
